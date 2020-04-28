a98c4adfe5b841b104092122a7597d1afdf2555d
Giovix92 <ggualtierone@gmail.com>
2020-04-27 20:20:06 -0400

    lavender: switch to official

--- revengeos_lavender.mk
+++ revengeos_lavender.mk
@@ -9,10 +9,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-TARGET_BOOT_ANIMATION_RES := 1080
-
 # Inherit from lavender device
 $(call inherit-product, device/xiaomi/lavender/device.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+REVENGEOS_BUILDTYPE := OFFICIAL
 
 # Inherit dalvik from native
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
