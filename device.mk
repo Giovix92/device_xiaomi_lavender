e25079f01a3a1ec68bc42aa596a3584f6ab4ad00
Giovix92 <ggualtierone@gmail.com>
2020-04-27 20:22:03 -0400

    lavender: drop CameraGo

--- device.mk
+++ device.mk
@@ -95,8 +95,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
-    vendor.qti.hardware.camera.device@1.0 \
-    CameraGo
+    vendor.qti.hardware.camera.device@1.0
 
 # Codec2 modules
 PRODUCT_PACKAGES += \
@@ -463,6 +462,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libnl \
     libwfdaac
-    
+
 PRODUCT_BOOT_JARS += \
     WfdCommon
